e0a50b90e0da5fdc0f23bcdea0bad732b66cf988
glztl <t55t555@163.com>
2026-06-26 17:11:15 +0800

    first commit

---
+++ agent-manage/src/main/java/com/tiny/agentplatform/manage/AgentManageApplication.java
@@ -0,0 +1,13 @@
+package com.tiny.agentplatform.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentManageApplication.class, args);
+    }
+
+}
---
+++ agent-manage/src/test/java/com/tiny/agentplatform/manage/AgentManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiny.agentplatform.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
